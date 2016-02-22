446f0fe036066c81192a10d50938d8ccf7d162da
stang149 <stang149@users.noreply.github.com>
2016-02-21 19:21:24 -0500

    Added files via upload

---
+++ client/app/main/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('3601S16Lab5JsonDataProcessingApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController',
+        controllerAs: 'main'
+      });
+  });
