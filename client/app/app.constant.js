722339a2eeeb81bb242aaef51b0b698134978910
stang149 <stang149@users.noreply.github.com>
2016-02-21 19:22:05 -0500

    Added files via upload

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('3601S16Lab5JsonDataProcessingApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
---
+++ client/app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('3601S16Lab5JsonDataProcessingApp', [
+  '3601S16Lab5JsonDataProcessingApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io',
+  'ui.router',
+  'ui.bootstrap'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
